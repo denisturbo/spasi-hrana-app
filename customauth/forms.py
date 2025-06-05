851aff68919ddd486f7913aa28a593a503dd55c3
denisturbo <140399445+denisturbo@users.noreply.github.com>
2025-06-05 18:18:15 +0300

    style:Reworked the Signup for business & customer. Also side nav and main for business account

--- customauth/forms.py
+++ customauth/forms.py
@@ -3,17 +3,17 @@ from django.forms import CharField, EmailField, ChoiceField
 from django.forms.widgets import TelInput, PasswordInput, RadioSelect, TextInput, EmailInput
 from customauth.models import BusinessUser, BaseSpasiHranaUser, CustomerUser
 
-class CustomLoginForm(AuthenticationForm):
+class CustomLoginForm(AuthenticationForm): #Business
     username = EmailField(
         label=("Потребителско име"),
-        widget=EmailInput(attrs={"autocomplete": "email", "placeholder": "Твоят email", "class": "bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5", "autofocus": True})
+        widget=EmailInput(attrs={"autocomplete": "email", "placeholder": "Твоят email", "class": "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5", "autofocus": True})
     )
 
     
     password = CharField(
         label=("Парола"),
         strip=False,
-        widget=PasswordInput(attrs={"placeholder": "••••••••",  "class":"bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5", "required": "" ,"autocomplete": "current-password"}),
+        widget=PasswordInput(attrs={"placeholder": "••••••••",  "class":"bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5", "required": "" ,"autocomplete": "current-password"}),
     )
 
     error_messages = {
@@ -24,21 +24,13 @@ class CustomLoginForm(AuthenticationForm):
         "inactive": ("Този акаунт изисква администраторско удобрение."),
     }
 
-class CustomerSignupForm(UserCreationForm):
-    first_name = CharField(
-                label=("Първо име"),
-                max_length=50, widget=TextInput(attrs={"placeholder": "Иван", "class": "bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5", "autofocus": True})
-                                    )
-    last_name = CharField(
-                label=("Фамилно име"),
-                max_length=50, widget=TextInput(attrs={"placeholder": "Петров", "class": "bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5", "autofocus": True})
-                                    )
 
+class CustomBaseSignupForm(UserCreationForm):
     password1 = CharField(
         label="Парола",
         widget=PasswordInput(attrs={
             "placeholder": "••••••••",
-            "class": "bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5",
+            "class": "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5",
             "autocomplete": "new-password"
         })
     )
@@ -46,18 +38,29 @@ class CustomerSignupForm(UserCreationForm):
         label="Повтори паролата",
         widget=PasswordInput(attrs={
             "placeholder": "••••••••",
-            "class": "bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5",
+            "class": "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5",
             "autocomplete": "new-password"
         })
     )
 
 
+class CustomerSignupForm(CustomBaseSignupForm):
+    first_name = CharField(
+                label=("Първо име"),
+                max_length=50, widget=TextInput(attrs={"placeholder": "Иван", "class": "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5", "autofocus": True})
+                                    )
+    last_name = CharField(
+                label=("Фамилно име"),
+                max_length=50, widget=TextInput(attrs={"placeholder": "Петров", "class": "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5", "autofocus": True})
+                                    )
+
+
     class Meta:
         model = BaseSpasiHranaUser
         fields = ['username', 'email', 'phone_number']
-        widgets = {'username':TextInput(attrs={"placeholder": "Потребителско име", "class": "bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5", "autofocus": True}),
-                   'email': EmailInput(attrs={"autocomplete": "email", "placeholder": "Вашият email", "class": "bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5", "autofocus": True}),
-                'phone_number': TelInput(attrs={"placeholder": "Тел. номер", "class": 'bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5'})
+        widgets = {'username':TextInput(attrs={"placeholder": "Потребителско име", "class": "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5", "autofocus": True}),
+                   'email': EmailInput(attrs={"autocomplete": "email", "placeholder": "Вашият email", "class": "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5", "autofocus": True}),
+                'phone_number': TelInput(attrs={"placeholder": "Тел. номер", "class": 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'})
 
 }
 
@@ -69,29 +72,28 @@ class CustomerSignupForm(UserCreationForm):
             CustomerUser.objects.create(
                 user=user,
                 first_name=self.cleaned_data['first_name'],
-                last_name=self.cleaned_data['last_name']
-            )
+                last_name=self.cleaned_data['last_name'],            )
         return user
 
 
-class BusinessSignupForm(CustomerSignupForm):
+class BusinessSignupForm(CustomBaseSignupForm):
     business_name = CharField(
                 label=("Име на бизнес"),
-max_length=50, widget=TextInput(attrs={"placeholder": "Баничарницата", "class": "bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5", "autofocus": True})
+max_length=50, widget=TextInput(attrs={"placeholder": "Баничарницата", "class": "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5", "autofocus": True})
     )
     location = CharField(
                 label=("Адрес"),
-max_length=50, widget=TextInput(attrs={"placeholder": "ул. Лилия 1", "class": "bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5", "autofocus": True})
+max_length=50, widget=TextInput(attrs={"placeholder": "ул. Лилия 1", "class": "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5", "autofocus": True})
     )
     business_type = ChoiceField(choices=BusinessUser.BUSINESS_TYPE_CHOICES,
                                 widget=RadioSelect(attrs={"class": "w-4 h-4 text-orange-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-orange-500"}))
 
     class Meta:
         model = BaseSpasiHranaUser
-        fields = ['username', 'email']
-        widgets = {'username':TextInput(attrs={"placeholder": "Потребителско име", "class": "bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5", "autofocus": True}),
-                'email': EmailInput(attrs={"autocomplete": "email", "placeholder": "Твоят email", "class": "bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5", "autofocus": True}),
-                'phone_number': TelInput(attrs={"placeholder": "Тел. номер", "class": 'bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5'})
+        fields = ['username', 'email', 'phone_number']
+        widgets = {'username':TextInput(attrs={"placeholder": "Потребителско име", "class": "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5", "autofocus": True}),
+                'email': EmailInput(attrs={"autocomplete": "email", "placeholder": "Твоят email", "class": "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5", "autofocus": True}),
+                'phone_number': TelInput(attrs={"placeholder": "Тел. номер", "class": 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'})
 
 }
 
@@ -105,7 +107,8 @@ max_length=50, widget=TextInput(attrs={"placeholder": "ул. Лилия 1", "cla
                 user=user,
                 business_name=self.cleaned_data['business_name'],
                 location=self.cleaned_data['location'],
-                business_type=self.cleaned_data['business_type']
+                business_type=self.cleaned_data['business_type'],
+
             )
         return user
     
