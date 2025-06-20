484d66c59d0419a83aaf990d2e3b6c5ea2516aba
denisturbo <140399445+denisturbo@users.noreply.github.com>
2025-06-20 18:09:47 +0300

    fix: changed the namespace method

--- listings/urls.py
+++ listings/urls.py
@@ -2,6 +2,7 @@
 from django.urls import path
 from .views import create, InfoTableList
 
+app_name = "listings"
 
 urlpatterns = [
     path('create-listing/', create, name='create'),
--- promocodes/urls.py
+++ promocodes/urls.py
@@ -2,6 +2,7 @@
 from django.urls import path
 # from .views import create_promocode, InfoTableList
 
+app_name = 'promocodes'
 
 urlpatterns = [
     # path('create-promocode/', create_promocode, name='create'),
