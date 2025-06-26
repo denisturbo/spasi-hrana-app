96db54f7256d8748419c3e55213a738e2efe11b6
denisturbo <140399445+denisturbo@users.noreply.github.com>
2025-06-26 13:28:33 +0300

    Converted Create Promocodes to CBV

--- promocodes/urls.py
+++ promocodes/urls.py
@@ -1,11 +1,11 @@
 
 from django.urls import path
-from .views import create,InfoTableList
+from .views import CreatePromocode,InfoTableList
 
 app_name = 'promocodes'
 
 urlpatterns = [
-    path('create-promocode/', create, name='create'),
+    path('create-promocode/', CreatePromocode.as_view(), name='create'),
     path('promocode-infotable/', InfoTableList.as_view(), name='infotable')
 
 ]
--- promocodes/views.py
+++ promocodes/views.py
@@ -2,25 +2,41 @@ from django.shortcuts import render, redirect
 from promocodes.forms import PromocodeCreation
 from promocodes.models import Promocode
 from customauth.models import BusinessUser
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.shortcuts import get_object_or_404
+from django.urls import reverse_lazy
 
 # Create your views here.
 
-def create(request):
-    if request.method == "POST":
-        form = PromocodeCreation(request.POST)
-        if form.is_valid():
-            new_promo = form.save(commit=False)
-            new_promo.connection = request.user.businessuser
-            new_promo.save()
-            return redirect('/')
-    else:
-        form = PromocodeCreation()
+# def create(request):
+#     if request.method == "POST":
+#         form = PromocodeCreation(request.POST)
+#         if form.is_valid():
+#             new_promo = form.save(commit=False)
+#             new_promo.connection = request.user.businessuser
+#             new_promo.save()
+#             return redirect('/')
+#     else:
+#         form = PromocodeCreation()
+#
+#
+#     return render(request, "business/promocodes/create.html", {"form": form})
 
 
-    return render(request, "business/promocodes/create.html", {"form": form})
+class CreatePromocode(PermissionRequiredMixin, CreateView):
+    permission_required = ('customauth.can_create_listing')
+    model = Promocode
+    template_name = 'business/promocodes/create.html'
+    form_class = PromocodeCreation
+    success_url = reverse_lazy('index')
+
+    def form_valid(self, form):
+        new_list = form.save(commit=False)
+        new_list.connection = self.request.user.businessuser
+        new_list.save()
+        return super().form_valid(form)
+
 
 
 class InfoTableList(PermissionRequiredMixin, ListView):
