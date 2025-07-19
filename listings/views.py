bbcc4863e9c1c6bf4635e342be80e1ffc8fbd1b6
denisturbo <140399445+denisturbo@users.noreply.github.com>
2025-07-19 18:14:14 +0300

    fix: HTMX SPA-Like deletion on business data table, Custom Paginator in listings/views.py

--- listings/views.py
+++ listings/views.py
@@ -1,3 +1,5 @@
+from django.core.paginator import Paginator
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
 from django.template.response import TemplateResponse
 
@@ -42,18 +44,44 @@ class EditListing(PermissionRequiredMixin, UpdateView):
         new_list.save()
         return super().form_valid(form)
 
+
 class DeleteListing(PermissionRequiredMixin, DeleteView):
     permission_required = ('customauth.can_create_listing')
     model = Listing
     success_url = reverse_lazy('listings:infotable')
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
 
+        if request.htmx:
+            # Implementirame Paginatora ot MultipleObjectMixin zashtoto v DeleteView go nqma..
+            current_page = request.GET.get('page', 1)
+            self.object.delete()
 
-def delete_list(request, pk):
-    listing = get_object_or_404(Listing, pk=pk, connection=request.user.businessuser)
-    listing.delete()
-    listing_list = Listing.objects.filter(connection=request.user.businessuser)
+            queryset = Listing.objects.filter(connection=self.request.user.businessuser)
+            paginate_by = 10
+            paginator = Paginator(queryset, paginate_by)
+            page_obj = paginator.get_page(current_page)
 
-    return render(request, 'htmx-partials/listing_table_partial.html', {"object_list": listing_list})
+            context = {
+                'object_list': page_obj.object_list,
+                'page_obj': page_obj,
+                'is_paginated': page_obj.has_other_pages(),
+                'paginator': paginator,
+                }
+            return render(request, 'htmx-partials/listing_table_partial.html', context)
+        return super().delete(request, *args, **kwargs)
+
+    def post(self, request: HttpRequest, *args, **kwargs):
+        #Post-a e prezapisan samo zaradi if-a da proverim dali zaqvkata se pravi ot HTMX (delete method)
+        self.object = self.get_object()
+        form = self.get_form()
+
+        if request.htmx:
+            return self.delete(request, *args, **kwargs)
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
 
 class InfoTableList(PermissionRequiredMixin, ListView):
     permission_required = ('customauth.can_create_listing')
