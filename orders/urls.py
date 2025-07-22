b5da22d824362bb01668c8c41533ecbe3bcd6e67
denisturbo <140399445+denisturbo@users.noreply.github.com>
2025-07-22 17:42:04 +0300

    feat; Order <-> Customer <-> Business logic done. Now business can change the status of the listing

--- orders/urls.py
+++ orders/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import CreateOrder
+from .views import CreateOrder, OrdersTableList,CompleteOrder
 
 app_name = "orders"
 
 urlpatterns = [
     path('create/<int:pk>/', CreateOrder.as_view(), name='create'),
+    path('table/', OrdersTableList.as_view(), name='table'),
+    path('complete/<int:pk>/', CompleteOrder.as_view(), name='complete')
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,10 +1,13 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import View
+from django.urls import reverse_lazy
+from django.views.generic import View, ListView, UpdateView
 
 from hranaapp.mixins import CreatedUpdatedAtMixin
 from listings.models import Listing
 from orders.models import Order
+from spasihrana.requests import HttpRequest
 
 
 # Create your views here.
@@ -27,4 +30,51 @@ class CreateOrder(LoginRequiredMixin, CreatedUpdatedAtMixin, View):
             'listing': listing,
         })
 
+class OrdersTableList(PermissionRequiredMixin, ListView):
+    permission_required = ('customauth.can_create_listing')
+    paginate_by = 10
+    model = Order
+    template_name = 'business/orders/data-table.html'
 
+    def get_queryset(self):
+        return Order.objects.filter(business=self.request.user.businessuser, listing__status='ordered')
+
+    def get(self, request: HttpRequest, *args, **kwargs):
+        self.object_list = self.get_queryset()
+        context = self.get_context_data()
+
+        if request.htmx:
+            print('vliza')
+            return render(request, "htmx-partials/order_table_partial.html", context)
+        else:
+            return render(request, self.template_name, context)
+
+
+class CompleteOrder(PermissionRequiredMixin, UpdateView):
+    permission_required = ('customauth.can_create_listing')
+    model = Order
+    success_url = reverse_lazy('orders:table')
+
+    def post(self, request: HttpRequest, *args, **kwargs):
+        self.object = self.get_object()
+
+        if request.htmx:
+            current_page = request.GET.get('page', 1)
+            print(self.object.listing.status, "IMA LI NQKOI")
+            self.object.listing.status = 'completed'
+            self.object.listing.save()
+            print(self.object.listing.status, "IMA LI NQKOI")
+
+            queryset = Order.objects.filter(business=self.request.user.businessuser, listing__status='ordered')
+            paginate_by = 10
+            paginator = Paginator(queryset, paginate_by)
+            page_obj = paginator.get_page(current_page)
+
+            context = {
+                'object_list': page_obj.object_list,
+                'page_obj': page_obj,
+                'is_paginated': page_obj.has_other_pages(),
+                'paginator': paginator,
+                }
+            return render(request, 'htmx-partials/order_table_partial.html', context)
+        return super().post(request, *args, **kwargs)
